78f0f03118004f7efc2cb5674189915721522b05
leonardogolinelli <leogolinelli@gmail.com>
2024-08-01 18:06:31 +0200

    small fix

--- utils.py
+++ utils.py
@@ -1,12 +1,9 @@
 #!/home/leonardo/miniconda3/envs/numpy_env/bin/python
 def draw_matrix_alignment(matrix, coordinates, key, top_sequence, bottom_sequence):
-    # Step 1: Ensure the matrix elements are integers
     matrix = matrix.astype(int)
     
-    # Step 2: Convert the matrix to a string representation
     str_matrix = matrix.astype(str)
     
-    # Step 3: Update the coordinates list
     coords = coordinates.copy()
     i, j = key.split(",")
     i, j = int(i), int(j)
